4f296e5bec7e9affa2857c27879a17f196924125
akshay-trika <akshay.t@trikatechnologies.com>
2023-09-11 11:23:54 +0530

    added the service for table

---
+++ node/clients/contact.ts
@@ -0,0 +1,42 @@
+import type { InstanceOptions, IOContext } from '@vtex/api'
+import { ExternalClient, Apps } from '@vtex/api'
+
+
+export default class Contact extends ExternalClient {
+  private setting: any | boolean = false
+  constructor(context: IOContext, options?: InstanceOptions) {
+    super(`http://trika.vtexcommercestable.com.br`, context, {
+      ...options,
+    })
+  }
+  public async getContactDetails() {
+    const contactData = await this.http.get<Promise<any>>(
+      `/api/dataentities/YF/search?_fields=firstname,lastname,email,subject,message,id,age&_schema=akshay`,
+      await this.getHeaders()
+    )
+
+    return contactData
+  }
+
+
+
+  private async getHeaders() {
+    const app = new Apps(this.context)
+    this.setting = await app.getAppSettings(process.env.VTEX_APP_ID ?? '')
+    console.log(this.setting)
+    return {
+      headers: {
+        'Content-type': 'application/json',
+        'X-VTEX-API-AppKey': this.setting?.apiKey,
+        'X-VTEX-API-AppToken': this.setting?.appToken
+      },
+    }
+  }
+  public async getCaptchadetails() {
+    const app = new Apps(this.context)
+    let settings = await app.getAppSettings(process.env.VTEX_APP_ID ?? '')
+    return {
+      sitekey:settings.recaptchaKey
+    }
+  }
+}
